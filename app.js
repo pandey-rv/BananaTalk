9eb104a7ea1e33d1c8441b9dba3f21e60074dcb9
pandey-rv <123pandeyravi@gmail.com>
2021-07-23 16:01:49 +0530

    json response

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var clickBtn = document.querySelector('#btn-translate');
 var userText = document.querySelector('#userText');
 var outputSec = document.querySelector('#output-div');
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-
-
+function setTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
+}
 
 function clickHandler() {
-    outputSec.innerText = userText.value;
+    var inputText = userText.value;
+
+    fetch(setTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+
 }
 
 clickBtn.addEventListener('click', clickHandler)
